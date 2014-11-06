86e84890b2e09c90ad75cba19f28fb4baceae272
Robert Lowe <robert@iblargz.com>
2014-11-05 20:38:51 -0500

    Publishing 2.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'robertlowe:meteor-phaser',
   summary: 'Phaser.io packaged for meteor',
-  version: '2.0.0',
+  version: '2.0.1',
   git: 'https://github.com/robertlowe/meteor-phaser.git'
 });
 
