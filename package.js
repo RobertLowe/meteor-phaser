619bab447b45caa3c9f81cecb4aa57d84d1118e1
Robert Lowe <robert@iblargz.com>
2014-11-05 19:46:13 -0500

    Fixes for 1.0

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'robertlowe:meteor-phaser',
   summary: 'Phaser.io packaged for meteor',
-  version: '1.0.0_1',
+  version: '1.0.0_2',
   git: 'https://github.com/robertlowe/meteor-phaser.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('phaser/build/phaser.js', 'client');
-  api.export(['Pixi', 'Phaser'], 'client');
+  api.addFiles('phaser/build/phaser.js', 'client', {bare: true});
+  api.export('PIXI', 'client');
+  api.export('Phaser', 'client');
 });
 
